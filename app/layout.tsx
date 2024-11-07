6b6c30fad78c22477a38a459378f9162e3b9764c
Christos Uster <christos.uster@gmail.com>
2024-11-07 17:07:02 +0600

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -31,7 +31,7 @@ export default function RootLayout({
             Developed by{" "}
             <a
               className="hover:underline hover:underline-offset-2 font-semibold hover:text-yellow-400"
-              href="https://christosuster.me/"
+              href="https://christosuster.dev/"
               target="_blank"
               rel="noopener noreferrer"
             >
